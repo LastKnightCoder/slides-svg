3ef52d216e1c178456d79c367acc73de55a41677
lastknighcoder <2223106858@qq.com>
2022-10-14 09:29:20 +0800

    ~~~

---
+++ layouts/bg.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="cover">
+    <div class="title">
+      <h1>{{ $slidev.configs.title }}</h1>
+    </div>
+    <div class="details">
+      <span>{{ $slidev.configs.author }}</span>
+      <span>{{ $slidev.configs.time.slice(0,10) }}</span>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.cover {
+  width: 100%;
+  height: 100%;
+  background: url(/cover8.jpg) center/cover no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+
+.title {
+  transform: translateY(25px);
+}
+
+.title h1 {
+  color: #29323c;
+  font-size: 4rem;
+  font-weight: bold;
+}
+
+.details {
+  color: #29323c;
+  transform: translateY(25px);
+  font-size: 2.5rem;
+  font-weight: 600;
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+}
+</style>
